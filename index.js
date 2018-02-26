5ebce6ddd95bc88875878d69d68314f30ce8bbd8
lutherworker <lutherworker@gmail.com>
2018-02-26 10:44:50 +0800

    my first commit

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+//=============================== Dividers Line ===============================
+
+'use strict';
+
+// Imports dependencies and set up http server
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); // creates express http server
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
