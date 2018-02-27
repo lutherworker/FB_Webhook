dc2b3775735152ebf1a0f6242685b5013caa2efc
lutherworker <lutherworker@gmail.com>
2018-02-27 12:02:56 +0800

    20180227 001

--- index.js
+++ index.js
@@ -40,12 +40,38 @@ app.get('/webhook', function(request, response) {
 // Facebook Webhook
 // Used for verification
 app.get("/webhook", function (req, res) {
+  /*
   if (req.query["hub.verify_token"] === "this_is_my_token") {
     console.log("Verified webhook");
     res.status(200).send(req.query["hub.challenge"]);
   } else {
     console.error("Verification failed. The tokens do not match.");
     res.sendStatus(403);
+  }
+  */
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "this_is_my_token"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
   }
 });
 
