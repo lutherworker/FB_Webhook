1dfa71a96e6ff7392dbf4688eb3d30d5704cdd80
lutherworker <lutherworker@gmail.com>
2018-02-27 12:49:15 +0800

    20180227 003

--- index.js
+++ index.js
@@ -81,7 +81,14 @@ app.listen(app.get('port'), function() {
 
 app.post("/webhook", function (req, res) {
   let body = req.body;
+  
+  if (body.object === 'page') {
+    res.status(200).send('EVENT_RECEIVED for page');
+  } else {
+    res.status(200).send('EVENT_RECEIVED for not page');
+  }
 
+  /*
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
 
@@ -100,6 +107,7 @@ app.post("/webhook", function (req, res) {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
+  */
 });
 
 //=============================== Dividers Line ===============================
